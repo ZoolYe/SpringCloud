c4cafc661263d360f42489aeb7dd3e7fe415aaca
zoolye <www.iphone@foxmail.com>
2018-06-07 18:38:50 +0800

    feat:添加.gitignore文件

--- ribbon-consumer/src/main/java/zool/ribbonconsumer/service/HelloService.java
+++ ribbon-consumer/src/main/java/zool/ribbonconsumer/service/HelloService.java
@@ -1,8 +1,6 @@
 package zool.ribbonconsumer.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
