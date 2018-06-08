8208dc25e76ba07701f1b30e2d0282d52c2a3ab9
zoolye <www.iphone@foxmail.com>
2018-06-08 09:36:26 +0800

    feat:.gitignore

--- ribbon-consumer/src/main/java/zool/ribbonconsumer/service/HelloService.java
+++ ribbon-consumer/src/main/java/zool/ribbonconsumer/service/HelloService.java
@@ -1,11 +1,13 @@
 package zool.ribbonconsumer.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class HelloService {
 
     private final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(String.valueOf(getClass()));
@@ -23,8 +25,15 @@ public class HelloService {
     }
 
 
-    public String helloFallback(){
+    @HystrixCommand(fallbackMethod = "defalutFallback")
+    public String helloFallback() throws InterruptedException {
+        Thread.sleep(3000);
         return "error";
+    }
+
+    public String defalutFallback(){
+        log.info("error defalut");
+        return "error defalut";
     }
 
 }
